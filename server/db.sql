08dcf56c337da58485538b018b4184e3a9e5e3df
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-06 12:27:38 -0800

    add db entries and fake user

--- server/db.sql
+++ server/db.sql
@@ -9,3 +9,8 @@ CREATE TABLE users(
   user_email VARCHAR(255) NOT NULL,
   user_password VARCHAR(255) NOT NULL
 );
+
+-- insert fake users
+
+INSERT INTO users (user_name, user_email, user_password )
+VALUES ('Hope', 'hopevaughnwarren@gmail.com', 'hvw1980');
